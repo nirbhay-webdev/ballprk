15fd56bdb3c2f59aa6b4e55272675ede8932ec19
nirbhay-webdev <nirbhay@simpledatalabs.com>
2017-06-11 13:55:55 +0530

    Explore Page

--- src/views/AppView.jsx
+++ src/views/AppView.jsx
@@ -1,15 +1,13 @@
-import React,{ Component } from 'react';
-import AppBar              from 'material-ui/AppBar';
-import LeftNav             from '../components/leftNav';
-import AppContent          from '../components/AppContent';
-import ShoutOut            from '../components/Shoutout'
-import LinkSharing         from '../components/LinkSharing';
-import SocialMediaWidgets  from '../components/SocialMediaWidgets'
-import ContactImportWidgets from '../components/ContactImportWidgets'
-import FundsDisplay        from '../components/FundsDisplay';
-import Graph                from '../components/Graph';
-import MediaKit             from '../components/MediaKit'
+import React,{ Component }        from 'react';
+import AppBar                     from 'material-ui/AppBar';
+import AppContent                 from '../components/AppContent';
+import SearchBar                  from '../components/SearchBar';
+import AthleteRecommendationBox   from '../components/AthleteRecommendationBox';
+import TopCategories              from '../components/TopCategories';
+import TopPosts                   from '../components/TopPosts'
+import POST_LIST_DATA             from '../data/RECOMMENDED_ATHELETES_DUMMY';
 var FontAwesome = require('react-fontawesome');
+
 const AppBar_Config = {
     showMenuIconButton: false,
     className: 'navBar fixedAtTop' 
@@ -20,24 +18,17 @@ class AppView extends Component {
     constructor(props) {
       super(props);
     }
-
+ 
     render() {
         return(
             <div id="App">
                 <NavBar/>
-                <div style={{ width: '100%', height: '100vh', position: 'relative', float: 'left' }}>
-                    <LeftNav className="leftNav" />
-                    <AppContent className="appContent">
-                        <ShoutOut user={{name: 'Herbert'}} />
-                        <LinkSharing id="link" url={ "http://ballprk.com/invite/affilitate-god.12" }/>
-                        <SocialMediaWidgets />
-                        <hr style={{ width: '300px', margin: '10px 0px 20px 0px', float: 'left', transform: 'translate(-50%)', left: '50%', position: 'relative' }}/>
-                        <ContactImportWidgets />
-                        <FundsDisplay id="funds"/>
-                        <Graph id="stats"/>
-                        <MediaKit />
-                    </AppContent>
-                </div>
+                <AppContent className="appContent" style={ { left: '0px', width: '100%' } }>
+                  <SearchBar />
+                  <AthleteRecommendationBox list={ POST_LIST_DATA } />
+                  <TopCategories list={[{},{},{},{},{},{}]} />
+                  <TopPosts list={ [{},{},{},{},{},{}] } />
+                </AppContent>
             </div>
         );
     }
